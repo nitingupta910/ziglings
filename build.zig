nitingupta910/ziglings

commit dbd42bb6488d4493a481951f45104dcf5b63bb2b

Cleaning up zig build output
* Only show actual Zig compiler errors, not build internals to
  confused and dismay.

* Remove advanced usage instructions not needed in normal
  operation.

build.zig
@@ -107,13 +107,14 @@ pub fn build(b: *Build) !void {
     }
 
     const logo =
-        \\
         \\         _       _ _
         \\     ___(_) __ _| (_)_ __   __ _ ___
         \\    |_  | |/ _' | | | '_ \ / _' / __|
         \\     / /| | (_| | | | | | | (_| \__ \
         \\    /___|_|\__, |_|_|_| |_|\__, |___/
         \\           |___/           |___/
+        \\
+        \\    "Look out! Broken programs below!"
         \\
         \\
     ;
@@ -255,18 +256,24 @@ const ZiglingStep = struct {
 
         const exe_path = self.compile(prog_node) catch {
             if (self.exercise.hint) |hint|
-                print("\n{s}HINT: {s}{s}", .{ bold_text, hint, reset_text });
+                print("\n{s}Ziglings hint: {s}{s}", .{ bold_text, hint, reset_text });
 
             self.help();
-            std.os.exit(1);
+
+            // NOTE: Returning 0 'success' status because the *exercise* failed,
+            // but Ziglings did not. Otherwise the learner will see this message:
+            //   "error: the following build command failed with exit code 1:..."
+            std.os.exit(0);
         };
 
         self.run(exe_path, prog_node) catch {
             if (self.exercise.hint) |hint|
-                print("\n{s}HINT: {s}{s}", .{ bold_text, hint, reset_text });
+                print("\n{s}Ziglings hint: {s}{s}", .{ bold_text, hint, reset_text });
 
             self.help();
-            std.os.exit(1);
+
+            // NOTE: See note above!
+            std.os.exit(0);
         };
     }
 
@@ -393,11 +400,12 @@ const ZiglingStep = struct {
                     print("\n", .{});
                 },
                 error.ZigIPCError => {
-                    print("{s}{s}: The following command failed to communicate the compilation result:{s}\n", .{
-                        red_text, self.exercise.main_file, reset_text,
-                    });
-                    for (argv) |v| print("{s} ", .{v});
-                    print("\n", .{});
+                    // Commenting this out for now. It always shows up when compilation fails.
+                    //print("{s}{s}: The following command failed to communicate the compilation result:{s}\n", .{
+                    //    red_text, self.exercise.main_file, reset_text,
+                    //});
+                    //for (argv) |v| print("{s} ", .{v});
+                    //print("\n", .{});
                 },
                 else => {
                     print("{s}{s}: Unexpected error: {s}{s}\n", .{
@@ -517,20 +525,22 @@ const ZiglingStep = struct {
 
     fn help(self: *ZiglingStep) void {
         const path = self.exercise.main_file;
-        const key = self.exercise.key();
 
         print("\n{s}Edit exercises/{s} and run 'zig build' again.{s}\n", .{
             red_text, path, reset_text,
         });
 
-        const format =
-            \\
-            \\{s}To compile only this exercise, you can also use this command:{s}
-            \\{s}zig build -Dn={s}{s}
-            \\
-            \\
-        ;
-        print(format, .{ red_text, reset_text, bold_text, key, reset_text });
+        // NOTE: The README explains this "advanced feature" if anyone wishes to use
+        //       it. Otherwise, beginners are thinking they *have* to do this.
+        //const key = self.exercise.key();
+        //const format =
+        //    \\
+        //    \\{s}To compile only this exercise, you can also use this command:{s}
+        //    \\{s}zig build -Dn={s}{s}
+        //    \\
+        //    \\
+        //;
+        //print(format, .{ red_text, reset_text, bold_text, key, reset_text });
     }
 
     fn printErrors(self: *ZiglingStep) void {
@@ -687,10 +697,12 @@ const exercises = [_]Exercise{
         .output = "Hello world!",
         .hint =
         \\DON'T PANIC!
-        \\Read the error above.
-        \\See how it has something to do with 'main'?
-        \\Open up the source file as noted and read the comments.
-        \\You can do this!
+        \\Read the compiler messages above. (Something about 'main'?)
+        \\Open up the source file as noted below and read the comments.
+        \\
+        \\(Hints like these will occasionally show up, but for the
+        \\most part, you'll be taking directions from the Zig
+        \\compiler itself.)
         \\
         ,
     },

commit 3964d25509266fbd128d7f7d281f14e109460a21

Restore unit tests
Commit dbd42bb (Cleaning up zig build output) broke the unit test.

Always use exit code 2, instead of 0.  This is the exit code used by the
build runner to notify the compiler to not report any further
diagnostics.

Move the Ziglings logo from the `build` function scope to the global
scope, and make it public so that tests.zig can use it to find the
number of lines to skip, instead of using an hard coded value.

Fixes #295

build.zig
@@ -74,9 +74,22 @@ pub const Exercise = struct {
     }
 };
 
+pub const logo =
+    \\         _       _ _
+    \\     ___(_) __ _| (_)_ __   __ _ ___
+    \\    |_  | |/ _' | | | '_ \ / _' / __|
+    \\     / /| | (_| | | | | | | (_| \__ \
+    \\    /___|_|\__, |_|_|_| |_|\__, |___/
+    \\           |___/           |___/
+    \\
+    \\    "Look out! Broken programs below!"
+    \\
+    \\
+;
+
 pub fn build(b: *Build) !void {
     if (!compat.is_compatible) compat.die();
-    if (!validate_exercises()) std.os.exit(1);
+    if (!validate_exercises()) std.os.exit(2);
 
     use_color_escapes = false;
     if (std.io.getStdErr().supportsAnsiEscapeCodes()) {
@@ -106,19 +119,6 @@ pub fn build(b: *Build) !void {
         reset_text = "\x1b[0m";
     }
 
-    const logo =
-        \\         _       _ _
-        \\     ___(_) __ _| (_)_ __   __ _ ___
-        \\    |_  | |/ _' | | | '_ \ / _' / __|
-        \\     / /| | (_| | | | | | | (_| \__ \
-        \\    /___|_|\__, |_|_|_| |_|\__, |___/
-        \\           |___/           |___/
-        \\
-        \\    "Look out! Broken programs below!"
-        \\
-        \\
-    ;
-
     const healed = b.option(bool, "healed", "Run exercises from patches/healed") orelse
         false;
     const override_healed_path = b.option([]const u8, "healed-path", "Override healed path");
@@ -136,7 +136,7 @@ pub fn build(b: *Build) !void {
     if (exno) |n| {
         if (n == 0 or n > exercises.len - 1) {
             print("unknown exercise number: {}\n", .{n});
-            std.os.exit(1);
+            std.os.exit(2);
         }
         const ex = exercises[n - 1];
 
@@ -280,10 +280,10 @@ const ZiglingStep = struct {
 
             self.help();
 
-            // NOTE: Returning 0 'success' status because the *exercise* failed,
-            // but Ziglings did not. Otherwise the learner will see this message:
-            //   "error: the following build command failed with exit code 1:..."
-            std.os.exit(0);
+            // NOTE: Using exit code 2 will prevent the Zig compiler to print
+            // the message:
+            // "error: the following build command failed with exit code 1:..."
+            std.os.exit(2);
         };
 
         self.run(exe_path, prog_node) catch {
@@ -293,7 +293,7 @@ const ZiglingStep = struct {
             self.help();
 
             // NOTE: See note above!
-            std.os.exit(0);
+            std.os.exit(2);
         };
     }
 

test/tests.zig
@@ -175,7 +175,7 @@ pub fn addCliTests(b: *std.Build, exercises: []const Exercise) *Step {
         const cmd = b.addSystemCommand(&.{ b.zig_exe, "build", "-Dn=1" });
         const expect = exercises[0].hint orelse "";
         cmd.setName("zig build -Dn=1");
-        cmd.expectExitCode(1);
+        cmd.expectExitCode(2);
         cmd.addCheck(.{ .expect_stderr_match = expect });
 
         cmd.step.dependOn(case_step);
@@ -282,10 +282,11 @@ const CheckStep = struct {
         for (exercises) |ex| {
             if (ex.number() == 1 and self.has_logo) {
                 // Skip the logo.
+                const nlines = mem.count(u8, root.logo, "\n");
                 var buf: [80]u8 = undefined;
 
                 var lineno: usize = 0;
-                while (lineno < 8) : (lineno += 1) {
+                while (lineno < nlines) : (lineno += 1) {
                     _ = try readLine(stderr, &buf);
                 }
             }